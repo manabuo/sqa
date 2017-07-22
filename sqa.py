1b67879dcbeb845fc792350ac95c259b282275ac
mdrft <minato@mdrft.com>
2017-07-23 07:46:03 +0900

    update while

--- sqa.py
+++ sqa.py
@@ -10,7 +10,7 @@ q = []
 for i in range(m):
 	q.append(random.choice([-1,1],N))
 
-while G > 0.02:
+while G>0.02:
 	for j in range(N*m):
 		x = random.randint(N)
 		y = random.randint(m)
@@ -18,6 +18,6 @@ while G > 0.02:
 		dE += q[y][x]*(q[(m+y-1)%m][x]+q[(y+1)%m][x])*log(1/tanh(G/kT/m))/kT
 		if dE<0 or exp(-dE/kT)>random.rand():
 			q[y][x] = -q[y][x]
-	G = G*0.99
+	G*=0.99
 
 print(q)
