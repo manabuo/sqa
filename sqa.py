ef1b33f341564eeaac24ae53945f3a232305fc7a
mdrft <minato@mdrft.com>
2017-07-23 07:33:55 +0900

    update

--- sqa.py
+++ sqa.py
@@ -1,15 +1,24 @@
 from numpy import *
 
-N = 10
-kT = 5 
+N = 20
+kT = 0.02
+G = 5
+m = 10 
 jij = 1
+q = []
 
-q = random.choice([-1,1],N)
+for jj in range(m):
+	q.append(random.choice([-1,1],N))
+
+print(q)
 
 for i in range(550):
-	for j in range(500):
+	for j in range(N*m):
 		x = random.randint(N)
-		dE = jij*2*q[x]*(q[(N+x-1)%N]+q[(x+1)%N])
+		y = random.randint(m)
+		dE = (jij*2*q[y][x]*(q[y][(N+x-1)%N]+q[y][(x+1)%N]))/m
+		dE = q[y][x]*(q[(m+y-1)%m][x]+q[(y+1)%m][x])*log(1/tanh(G/kT/m))/kT
 		if dE<0 or exp(-dE/kT)>random.rand():
-			q[x] = -q[x]
-	kT = kT*0.99
+			q[y][x] = -q[y][x]
+	G = G*0.99
+print(q)
